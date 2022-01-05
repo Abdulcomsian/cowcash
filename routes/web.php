725d57c60c99571222375d7cba08c518b3612e18
obaidjani <muhammad.obaid@dazaran.com>
2022-01-05 16:47:47 +0500

    inital code are setup

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use PhpParser\Node\Expr\FuncCall;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', 'AdminController@index');
+Route::get('/dashboard', [AdminController::class, 'dashboard']);
+Route::get('/profile', [HomeController::class, 'profile']);
+
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
