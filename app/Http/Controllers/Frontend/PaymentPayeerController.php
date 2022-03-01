73351705b7a2a7e5717560aa8035ea19612aea5a
obaidjani <muhammad.obaid@dazaran.com>
2022-03-01 18:55:22 +0500

    per minut milk caluclation done

--- app/Http/Controllers/Frontend/PaymentPayeerController.php
+++ app/Http/Controllers/Frontend/PaymentPayeerController.php
@@ -21,7 +21,7 @@ class PaymentPayeerController extends Controller
         $pkgid = $request->package_id;
         $user = Auth::user();
         $user = $user->id;
-        $m_shop =  '1608608742';
+        $m_shop =  '1612874077';
         $m_orderid = mt_rand();
         $m_amount = number_format($request->purchase_sum, 2, '.', '');
         $m_curr = 'USD';
@@ -37,25 +37,6 @@ class PaymentPayeerController extends Controller
             $m_key
         );
         $sign = strtoupper(hash('sha256', implode(':', $arHash)));
-
-        $arParams = array(
-            'reference' => array(
-                'userid' => $user,
-            ),
-        );
-
-        // Encrypting additional parameters using AES-256-CBC (for >= PHP 7)
-        $iv = substr(hash('sha256',  $m_key), 0, 16);
-        $m_params = urlencode(base64_encode(openssl_encrypt(
-            json_encode($arParams),
-            'AES-256-CBC',
-            $m_key,
-            OPENSSL_RAW_DATA,
-            $iv
-        )));
-
-        // Adding parameters to the signature-formation array
-        $arHash[] = $m_params;
 
         if ($m_amount != 0) {
             try {
@@ -84,29 +65,22 @@ class PaymentPayeerController extends Controller
             }
         }
 
-        $arGetParams = array(
-            'm_shop' => $m_shop,
-            'm_orderid' => $m_orderid,
-            'm_amount' => $m_amount,
-            'm_curr' => $m_curr,
-            'm_desc' => $m_desc,
-            'm_sign' => $sign,
-            'm_params' => $m_params,
-            //'m_cipher_method' => 'AES-256-CBC-IV',
-            //'form[ps]' => '2609',
-            //'form[curr[2609]]' => 'USD',
-        );
 
-        return redirect()->to("https://payeer.com/merchant/?" . http_build_query($arGetParams) . "&lang=en");
+        return redirect()->to("https://payeer.com/merchant/?m_shop=$m_shop&m_orderid=$m_orderid&m_amount=$m_amount&m_curr=$m_curr&m_desc=$m_desc&m_sign=$sign&lang=en");
     }
 
     //user payoff
     public function payoff(Request $request)
     {
-        $godbarsum = $request->sum;
-        $urgoldbar = Auth::user()->withdraw;
-        if ($godbarsum > $urgoldbar) {
-            toastError('The amount of gold bars exceeds your account balance You have ' . $urgoldbar . ' gold bars (for withdrawal)');
+        $silverblocks = $request->silverblocks;
+        $ursilverblocks = Auth::user()->withdraw;
+        $crystal = Auth::user()->crystal;
+        $amount = $request->amount;
+        if ($silverblocks >  $ursilverblocks) {
+            toastError('The amount of Silver block exceeds your account balance You have ' .  $silverblocks . ' Silver Blocks (for withdrawal)');
+            return Redirect::back();
+        } elseif ($crystal < $amount) {
+            toastError('The amount of Silver block exceeds your account balance You have ' . $urgoldbar . ' Silver Blocks (for withdrawal)');
             return Redirect::back();
         } else {
             toastSuccess('Withdraw code working');
