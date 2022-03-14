a6d5675ca7cdda7f2ce790a110496120d5f2078a
Hamza <Hamza.Alam@accrualgroup.com>
2022-03-14 18:07:21 +0500

    buy Cow layout change

---
+++ resources/views/Frontend/includes/buyCowMenu.blade.php
@@ -0,0 +1,44 @@
+<div class="menuBox" style="background-position: inherit;">
+    <div class="menuList" style="padding: 0px !important;">
+        <div class="listView" style="margin: 0px !important;">
+            <div class="listdiv">
+                <a href="{{url('/home')}}">
+                    <img src="{{asset('frontend/assets/img/main.png')}}" alt="" style="width: 60px !important">
+                    <p>Main</p>
+                </a>
+            </div>
+            <div class="listdiv">
+                <a href="{{url('/payments')}}">
+                    <img src="{{asset('frontend/assets/img/paymentMenu.png')}}" alt="" style="width: 60px !important">
+                    <p>Payments</p>
+                </a>
+            </div>
+            <div class="listdiv">
+                <a href="{{url('/rules')}}">
+                    <img src="{{asset('frontend/assets/img/rules.png')}}" alt="" style="width: 60px !important">
+                    <p>Rules</p>
+                </a>
+            </div>
+        </div>
+        <div class="listView">
+            <div class="listdiv">
+                <a href="{{url('/about')}}">
+                    <img src="{{asset('frontend/assets/img/about.png')}}" alt="" style="width: 60px !important">
+                    <p>About</p>
+                </a>
+            </div>
+            <div class="listdiv">
+                <a href="{{url('account/calculate')}}">
+                    <img src="{{asset('frontend/assets/img/calculate.png')}}" alt="" style="width: 60px !important">
+                    <p>Calculate</p>
+                </a>
+            </div>
+            <div class="listdiv">
+                <a href="{{url('/support')}}">
+                    <img src="{{asset('frontend/assets/img/support.png')}}" alt="" style="width: 60px !important">
+                    <p>Support</p>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
