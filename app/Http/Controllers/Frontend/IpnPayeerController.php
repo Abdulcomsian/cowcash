4927c1b4d4bed69f90a6feafb6e5fef4eabc26ec
obaidjani <muhammad.obaid@dazaran.com>
2022-03-28 15:51:20 +0500

    chagnes done

--- app/Http/Controllers/Frontend/IpnPayeerController.php
+++ app/Http/Controllers/Frontend/IpnPayeerController.php
@@ -6,9 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\PackageTxn;
 use DB;
 use Carbon\Carbon;
-use Facade\Ignition\Support\Packagist\Package;
+use App\Models\Packages;
 
 /**
  * Class IpnPayeerController
@@ -23,7 +24,7 @@ class IpnPayeerController extends Controller
      */
     public function Payment_Success(Request $request)
     {
-        if ($_SERVER['REMOTE_ADDR'] != '37.59.221.230') return;
+        //if ($_SERVER['REMOTE_ADDR'] != '37.59.221.230') return;
         $m_key = '123';
         $m_shop = $request->get('m_shop');
         $m_orderid = $request->get('m_orderid');
@@ -33,31 +34,33 @@ class IpnPayeerController extends Controller
         $checksum = $request->get('m_sign');
         $user = Payment::select('user_id')->where('uid', '=', $m_orderid)->first();
 
-        if (isset($_POST['m_operation_id']) && isset($checksum)) {
-
+        if ($request->get('m_operation_id') && isset($checksum)) {
             $arHash = array($m_shop, $m_orderid, $m_amount, $m_curr, $m_desc, $m_key);
             $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
-
-            if ($checksum == $sign_hash && $_POST['m_status'] == 'success') {
+            
+            if ($request->get('m_status') == 'success') {
                 if (Payment::where('uid', '=', $m_orderid) && Payment::where('balance', '=', $m_amount)) {
                     try {
                         DB::beginTransaction();
                         $payment = Payment::where('uid', '=', $m_orderid)->first();
-                        if ($payment->status == 0) {
-                            $payment->status = 1;
+                        if ($payment->payment_status == 0) {
+                            $payment->payment_status = 1;
                             $payment->update();
 
                             //pkg status
                             $packageinsertdata = PackageTxn::where(['user_id' => $user->user_id, 'uid' => $m_orderid])->update(['payment_status' => 1]);
-                            $Packagedata = Package::where('id', $packageinsertdata->package_id)->first();
-                            if($Packagedata)
+                            if($packageinsertdata)
                             {
-                            //covert 40 percent of coinst to crystal 
-                            $crystals = $Packagedata->amount / 100 * 40;
-                            $addBalanceToUser = User::find($user->user_id);
-                            $addBalanceToUser->silver_coins += $Packagedata->coins_to_get;
-                            $addBalanceToUser->crystal += $crystals;
-                            $addBalanceToUser->update();
+                                $Packagedata = Packages::where('id', $packageinsertdata->package_id)->first();
+                                if($Packagedata)
+                                {
+                                //covert 40 percent of coinst to crystal 
+                                $crystals = $Packagedata->amount / 100 * 40;
+                                $addBalanceToUser = User::find($user->user_id);
+                                $addBalanceToUser->silver_coins += $Packagedata->coins_to_get;
+                                $addBalanceToUser->crystal += $crystals;
+                                $addBalanceToUser->update();
+                                }
                             }
                             else{
                                 
@@ -68,8 +71,7 @@ class IpnPayeerController extends Controller
                                 $addBalanceToUser->crystal += $crystals;
                                 $addBalanceToUser->update();
                             }
-                        }
-                        //working for referal
+                            //working for referal
                          if ($user) {
                                 //check parent
                             if ($user->referred_by != NULL) {
@@ -128,21 +130,22 @@ class IpnPayeerController extends Controller
                             }
                         }
                             //     return $user;
+                        }
+                        
                         DB::commit();
                     } catch (\PDOException $e) {
-
                         \Session::flash('message', "$e->getMessage()");
                         DB::connection()->getPdo()->rollBack();
                     }
                 }
             }
         }
-        return redirect()->action('ProfileController@index');
+         toastSuccess('Payment Success');
+        return redirect()->to('/home');
     }
 
     public function Payment_Fail(Request $request)
     {
-        if ($_SERVER['REMOTE_ADDR'] != '37.59.221.230') return;
         $m_key = '123';
         $m_shop = $request->get('m_shop');
         $m_orderid = $request->get('m_orderid');
@@ -154,7 +157,7 @@ class IpnPayeerController extends Controller
         Payment::where('uid', '=', $m_orderid)->delete();
         PackageTxn::where(['user_id' => $user->user_id, 'uid' => $m_orderid])->delete();
         toastError('Payment Failed');
-        return redirect()->action('ProfileController@index');
+       return redirect()->to('/home');
     }
 
     public function Payment_Status(Request $request)
